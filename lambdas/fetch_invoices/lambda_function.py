e96180673191153a9d7748ef6152ef5f2072ca3c
azfar-imtiaz <azfar.imtiaz@live.com>
2025-04-04 01:02:20 +0200

    Optimize this function to fetch only those invoices which don't already exist in DynamoDB. Add more logging.

--- lambdas/fetch_invoices/lambda_function.py
+++ lambdas/fetch_invoices/lambda_function.py
@@ -1,15 +1,23 @@
 import os
 import json
 import email
+from http.client import responses
+
 import boto3
 import imaplib
 import quopri
 import logging
 
+from collections import defaultdict
 from email.header import decode_header
+from email.message import Message
+from email.utils import parsedate_to_datetime
 from botocore.exceptions import ClientError
+from botocore.config import Config
 
 s3_client = boto3.client('s3')
+config = Config(retries={'max_attempts': 5, 'mode': 'adaptive'})
+dynamodb = boto3.resource('dynamodb', config=config)
 secret_client = boto3.client('secretsmanager')
 
 
@@ -47,10 +55,80 @@ def get_body_from_email(s):
     return decoding_string
 
 
-def get_s3_path(email, filename):
+def get_s3_path(email: str, filename: str):
     username = email.split('@')[0]
     filename = filename.replace(' ', '_')
     return f"rental-invoices/{username}/{filename}"
+
+
+def invoice_exists_in_dynamodb(current_month: int, current_year: int, invoice_dates: defaultdict) -> bool:
+    """
+    This function checks if an invoice with the current month and year exists in the DynamoDB table
+    """
+    """
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    response = table.query(
+        IndexName='due_date_year-due_date_month-index',
+        KeyConditionExpression=Key('due_date_year').eq(current_year) & Key('due_date_month').eq(current_month)
+    )
+    return len(response.get('Items', [])) > 0
+    """
+    return invoice_dates[current_year][current_month]
+
+
+def get_all_invoice_dates() -> defaultdict:
+    """
+    This function get the month and year for all invoices in the DynamoDB table, and returns them as a Dict
+    """
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+    response = table.scan(
+        ProjectionExpression="due_date_month, due_date_year"
+    )
+    invoices = response['Items']
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(
+            ProjectionExpression="due_date_month, due_date_year",
+            ExcluseiveStartKey=response['LastEvaluatedKey']
+        )
+
+    invoice_dates = defaultdict(lambda: defaultdict(lambda: False))
+    for invoice in invoices:
+        year = invoice['due_date_year']
+        month = invoice ['due_date_month']
+        invoice_dates[year][month] = True
+    return invoice_dates
+
+
+def extract_and_upload_invoice(msg: Message, invoice_count_added: bool, invoices_found: int, user: str) -> int:
+    subject = decode_string(msg['subject'])
+    if subject.strip().lower() == os.environ['EMAIL_SUBJECT']:
+
+        if not invoice_count_added:
+            invoices_found += 1
+            invoice_count_added = True
+
+        logging.info("Retrieving information from email...")
+        for part in msg.walk():
+            if part.get_content_type() == 'text/plain':
+                body_string = part.get_payload()
+                body = get_body_from_email(body_string)
+                logging.info(body)
+
+            if part.get_content_type() == "application/pdf":
+                filename = part.get_filename()
+                filename = decode_string(filename)
+                if filename:
+                    logging.info("Downloading " + filename + "...")
+                    file_content = part.get_payload(decode=True)
+                    s3_key = get_s3_path(user, filename)
+                    s3_client.put_object(
+                        Bucket=os.environ['S3_BUCKET'],
+                        Key=s3_key,
+                        Body=file_content
+                    )
+                    logging.info(f"{filename} uploaded to S3!")
+
+    return invoices_found
 
 
 def lambda_handler(event, context):
@@ -78,44 +156,36 @@ def lambda_handler(event, context):
 
     messages = []
     invoices_found = 0
+    invoice_dates = get_all_invoice_dates()
+    logging.info(f"Here are the invoice dates: {dict(invoice_dates)}")
     for num in mail_id_list:
         _, mail_data = my_mail.fetch(num, '(RFC822)')
         messages.append(mail_data)
 
-        for msg in messages[::-1]:
-            invoice_count_added = False
-            for response_part in msg:
-                if type(response_part) is tuple:
-                    my_msg = email.message_from_bytes((response_part[1]))
+    for msg in messages[::-1]:
+        invoice_count_added = False
+        for response_part in msg:
+            if type(response_part) is tuple:
+                my_msg = email.message_from_bytes((response_part[1]))
+                email_date = parsedate_to_datetime(my_msg['Date']) if my_msg['Date'] else None
 
-                    subject = decode_string(my_msg['subject'])
+                if email_date:
+                    logging.info(f"Email found for date: {email_date}")
+                    if not invoice_exists_in_dynamodb(email_date.month, email_date.year, invoice_dates):
+                        logging.info(f"Invoice doesn't exist for: {email_date.month} and {email_date.year}! Extracting and uploading...")
+                        invoices_found = extract_and_upload_invoice(
+                            my_msg,
+                            invoice_count_added,
+                            invoices_found,
+                            user
+                        )
+                    else:
+                        logging.info("Invoice already exists in S3!")
+                else:
+                    logging.warning(f"Unable to parse date from this email: {my_msg}")
 
-                    if subject.strip().lower() == os.environ['EMAIL_SUBJECT']:
-                        
-                        if not invoice_count_added:
-                            invoices_found += 1
-                            invoice_count_added = True
-
-                        logging.info("Retrieving information from email...")
-                        for part in my_msg.walk():
-                            if part.get_content_type() == 'text/plain':
-                                body_string = part.get_payload()
-                                body = get_body_from_email(body_string)
-                                logging.info(body)
-
-                            if part.get_content_type() == "application/pdf":
-                                filename = part.get_filename()
-                                filename = decode_string(filename)                                
-                                if filename:
-                                    logging.info(f"Downloading {filename}...")
-                                    file_content = part.get_payload(decode=True)
-                                    s3_key = get_s3_path(user, filename)
-                                    s3_client.put_object(
-                                        Bucket=os.environ['S3_BUCKET'],
-                                        Key=s3_key,
-                                        Body=file_content
-                                    )
-                                    logging.info(f"{filename} uploaded to S3!")
+    my_mail.close()
+    my_mail.logout()
 
     return {
         'statusCode': 200,
